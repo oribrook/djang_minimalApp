7af3ea73d5bae8aef1532c5e6800a02b2ba68c1f
ori_b <ori@ori>
2023-03-14 10:50:26 +0200

    change main.py location and content

---
+++ my_prj/main.py
@@ -0,0 +1,16 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_prj.settings")
+
+
+import django
+django.setup()
+
+
+# here you can use all django models/features etc
+
+# from django.contrib.auth.models import User
+# users = User.objects.all()
+
+# from my_app.models import *
+
+# ...
