f88f86a5a908446cd6882cdeeab5ef37e56eaa15
ori_b <ori@ori>
2023-04-30 08:20:26 +0300

    adding static-url to urlpatterns

--- my_prj/my_app/urls.py
+++ my_prj/my_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('file', views.save_file, name='file'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
